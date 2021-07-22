ae9509b55a4699508d9a37600717d5ebe997471d
herven <ocarus@hotmail.fr>
2021-07-22 19:09:11 +0200

    cleaning code

--- main.rb
+++ main.rb
@@ -11,7 +11,4 @@ cars = Calculation.cars(car_datas)
 rentals = Calculation.rentals(rental_datas)
 
 Calculation.outpout('data/outpout_1.json', Calculation.result(cars, rentals))
-Calculation.outpout('data/outpout_2.json', Calculation.result_2(cars, rentals))
-Calculation.outpout('data/outpout_3.json', Calculation.result_3(cars, rentals))
-Calculation.outpout('data/outpout_4.json', Calculation.result_4(cars, rentals))
 
